090855f0c15557e9f1fa8992d86e71d011bf101a
Julli Mayanne <jullimayanne9@gmail.com>
2022-10-02 18:36:01 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Declare vaiaveis com todos os tipos 
+// primitivos com uma inicializacao constate
+
+namespace ConsoleApp.Pratical
+{
+    class Program
+    {
+         static void Main (string[] args)
+        {
+            int  int1 = 10;
+            Console.Write(int1);
+               
+         }
+    }
+}
